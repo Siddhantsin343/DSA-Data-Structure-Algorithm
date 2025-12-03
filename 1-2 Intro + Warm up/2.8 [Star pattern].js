8e76962e915ef45daeea090f318feadd51a27320
siddhant <siddhantsin343@gmail.com>
2025-12-03 21:50:09 +0530

    - star pattern [4x4]

---
+++ 1-2 Intro + Warm up/2.8 [Star pattern].js
@@ -0,0 +1,14 @@
+//Star Pattern 
+
+// 4 x 4 star pattern n=4
+
+let n = 4
+    for(i=0; i<n; i++){
+        let row = " ";
+        for(j=0; j<n ; j++){
+            row = row + "*";
+        }
+        console.log(row, i)
+    }
+
+    
